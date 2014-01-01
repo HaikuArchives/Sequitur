939525e4e4457dfccb8324d8eec9e4bc1108068a
Puck Meerburg <puck@puckipedia.nl>
2014-01-01 23:42:09 +0100

    Move from .r to .rdef

--- ArpSrc/ArpKernel/AboutWindow.r
+++
@@ -1,5 +0,0 @@
-// Resources needed for the ArpAboutView and ArpAboutWindow classes.
-
-resource('jpeg',   1, "ArpLogo") {
-	read("AboutWindow/ArpAboutLogo.jpeg")
-}
--- ArpSrc/ArpKernel/ArpDocumentButtonRes.r
+++
@@ -1,5 +0,0 @@
-// Resources needed for the ArpAboutView and ArpAboutWindow classes.
-
-resource('bmp ',   1, "ArpDocumentButton") {
-	read("ArpDocumentButtonRes/ArpDocumentButton.bmp")
-}
